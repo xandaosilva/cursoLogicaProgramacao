4497d92f1f9b08b3441528ba230ed8966d517fdd
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-19 15:36:36 -0300

    Add HourContract class

--- vs2019/Course_worker/Course_worker/Entities/HourContract.cs
+++ vs2019/Course_worker/Course_worker/Entities/HourContract.cs
@@ -6,5 +6,24 @@ namespace Course_worker.Entities
 {
     class HourContract
     {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
     }
 }
