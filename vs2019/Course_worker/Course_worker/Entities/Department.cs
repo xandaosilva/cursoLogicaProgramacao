88372a8b8751a4f6959ac521caa159e2d6734edc
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-19 15:31:10 -0300

    Add department class

--- vs2019/Course_worker/Course_worker/Entities/Department.cs
+++ vs2019/Course_worker/Course_worker/Entities/Department.cs
@@ -6,5 +6,15 @@ namespace Course_worker.Entities
 {
     class Department
     {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
     }
 }
