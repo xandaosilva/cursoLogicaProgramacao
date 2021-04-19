a12c2d621f12318f4601b1d34b36d111c6eeba81
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-19 15:03:46 -0300

    Exemplo enum

---
+++ vs2019/Course_order/Course_order/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using Course_order.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_order.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ vs2019/Course_order/Course_order/Program.cs
@@ -0,0 +1,21 @@
+﻿using Course_order.Entities;
+using Course_order.Entities.Enums;
+using System;
+
+namespace Course_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order { Id = 1080, Moment = DateTime.Now, Status = OrderStatus.PendingPayment };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
